6cc107698b65051a65c4d493622b6df85dbab1ac
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-03-07 19:02:36 -0700

    cleaned up the code

--- designs.js
+++ designs.js
@@ -1,19 +1,7 @@
 // Shortcut to #pixelCanvas
 let gridTable = $("#pixelCanvas");
 
-// Function for when you click the submit button
-$("#submit").click(function(event) {
-  // When I click on the button, it won't refresh the page
-  event.preventDefault();
-
-  // Look for the values of the numbers and input them into the makeGrid() function
-  let inputHeight = $("#inputHeight").val();
-  let inputWidth = $("#inputWidth").val();
-  makeGrid(inputHeight, inputWidth);
-});
-
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid(height, width) {
   //Empty out the table
   gridTable.empty();
@@ -26,16 +14,27 @@ function makeGrid(height, width) {
   // Create the width of the grid
   for (let w = 0; w < width; w++) {
     $("tr").append("<td></td>");
+  }
 
   // Select a color and input the color on the box you click on
   $("td").mousedown(function(event) {
     let color = $("#colorPicker").val();
     $(this).css("background-color", color);
   });
+
   // Double click to remove the color.
   $("td").dblclick(function() {
     $(this).css("background-color", "transparent");
   });
-
-  }
 }
+
+// Function for when you click the submit button
+$("#submit").click(function(event) {
+  // When I click on the button, it won't refresh the page
+  event.preventDefault();
+
+  // Look for the values of the numbers and input them into the makeGrid() function
+  let inputHeight = $("#inputHeight").val();
+  let inputWidth = $("#inputWidth").val();
+  makeGrid(inputHeight, inputWidth);
+});
