c83500c3b899c15a2d30f76d8e566483931672cb
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-03-07 18:27:43 -0700

    input can now be used to create a grid using makeGrid()

--- designs.js
+++ designs.js
@@ -1,12 +1,24 @@
 // Select color input
 
+
+
 // Select size input
+
+$("#submit").click(function(event) {
+  // When I click on the button, it won't refresh the page
+  event.preventDefault();
+
+  // Look for the values of the numbers and input them into the makeGrid() function
+  let inputHeight = $("#inputHeight").val();
+  let inputWidth = $("#inputWidth").val();
+  makeGrid(inputHeight, inputWidth);
+});
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid(height, width) {
   // Shortcut to #pixelCanvas
-  const gridTable = $("#pixelCanvas");
+  let gridTable = $("#pixelCanvas");
   gridTable.empty();
 
   // Create the height of the grid
@@ -16,9 +28,6 @@ function makeGrid(height, width) {
 
   // Create the width of the grid
   for (let w = 0; w < width; w++) {
-    $('tr').append("<td></td>");
+    $("tr").append("<td></td>");
   }
-
 }
-
-makeGrid(10,10);
