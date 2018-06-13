cf59427174abac27cbe57f231e09927042513a12
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-06-12 22:52:01 -0600

    added click and double click for adding color

--- js/designs.js
+++ js/designs.js
@@ -1,57 +1,12 @@
-// // Shortcut to #pixelCanvas
-// let gridTable = $("#pixelCanvas");
-// let inputHeight = $("#inputHeight").val();
-// let inputWidth = $("#inputWidth").val();
-
-// // When size is submitted by the user, call makeGrid()
-// function makeGrid(height, width) {
-//   //Empty out the table
-//   gridTable.empty();
-
-//   // Create the height of the grid
-//   for (let h = 0; h < height; h++) {
-//     gridTable.append("<tr></tr>");
-//   }
-
-//   // Create the width of the grid
-//   for (let w = 0; w < width; w++) {
-//     $("tr").append("<td></td>");
-//   }
-
-//   // Select a color and input the color on the box you click on
-//   $("td").mousedown(function(event) {
-//     let color = $("#colorPicker").val();
-//     $(this).css("background-color", color);
-//   });
-
-//   // Double click to remove the color.
-//   $("td").dblclick(function() {
-//     $(this).css("background-color", "transparent");
-//   });
-// }
-
-// // By default, this will create a grid with the default values
-// makeGrid(inputHeight, inputWidth);
-
-// // Function for when you click the submit button
-// $("#submit").click(function(event) {
-//   // When I click on the button, it won't refresh the page
-//   event.preventDefault();
-
-//   // Look for the values of the numbers and input them into the makeGrid() function
-//     inputHeight = $("#inputHeight").val();
-//     inputWidth = $("#inputWidth").val();
-//     makeGrid(inputHeight, inputWidth);
-// });
-
 // // Shortcut to #pixelCanvas, #inputHeight, #inputWidth, #submit
 let gridTable = document.querySelector('#pixelCanvas');
 let inputHeight = document.querySelector('#inputHeight').value;
 let inputWidth = document.querySelector('#inputWidth').value;
 const submit = document.querySelector('#submit');
 
-// // When size is submitted by the user, call makeGrid()
+// When size is submitted by the user, call makeGrid()
 function makeGrid(height, width) {
+
   // Empty out the table
   gridTable.innerHTML = "";
 
@@ -66,6 +21,16 @@ function makeGrid(height, width) {
       tableRow.appendChild(tableColumn);
     }
   }
+
+  // Select a color and input the color on the box you click on
+  document.querySelector("td").addEventListener("click", function() {
+    let color = document.querySelector('#colorPicker').value;
+    this.style.backgroundColor = color;
+  });
+
+  document.querySelector("td").addEventListener("dblclick", function () {
+    this.style.backgroundColor = 'transparent';
+  });
 }
 
 // By default, this will create a grid with the default values
